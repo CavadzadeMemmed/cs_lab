c5b1f1850358fb07e7ee0a7704171dcab1bb07d4
CavadzadeMemmed <m.i.cavadzade2020@gmail.com>
2024-09-21 17:59:31 +0400

    update

--- exercise4.c
+++ exercise4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     int a, b, c, D, x1, x2;
@@ -6,8 +7,8 @@ int main(){
     scanf("%d%d%d", &a, &b, &c);
 
     D = b*b - 4*a*c;
-    x1 = (-b - D*0.5)/(2*a);
-    x2 = (-b + D*0.5)/(2*a);
+    x1 = (-b - pow(D, 0.5))/(2*a);
+    x2 = (-b + pow(D, 0.5))/(2*a);
 
     if (D > 0)
         printf("x1=%d, x2=%d", x1, x2);
