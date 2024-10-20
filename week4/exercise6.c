7ce5415b72e2b0d4ae8c54b06c215ef5fcd16f3b
CavadzadeMemmed <m.i.cavadzade2020@gmail.com>
2024-10-20 10:27:12 +0400

    new files

--- week4/exercise6.c
+++ week4/exercise6.c
@@ -17,16 +17,17 @@ int main() {
     for (int i = 0; i < n; i++) {
         int count = 1; 
 
-        //if (arr[i] != -1) {  
+        if (arr[i] != -1) {  
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] == arr[j]) {
                     count++;  
                     arr[j] = -1;  
                 }
-            //}
+            }
 
             printf("%d %d\n", arr[i], count);
         }
+        
     }
 
     return 0;
